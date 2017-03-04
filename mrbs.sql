86fc4e71e85a05dd56a61c0da970c0a9af76a47a
djbetho <ru.bugueno@gmail.com>
2017-03-04 01:00:53 -0300

    slq
    dsss

---
+++ mrbs.sql
@@ -0,0 +1,373 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-03-2017 a las 04:58:18
+-- Versión del servidor: 10.1.21-MariaDB
+-- Versión de PHP: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `mrbs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mrbs_area`
+--
+
+CREATE TABLE `mrbs_area` (
+  `id` int(11) NOT NULL,
+  `disabled` tinyint(1) NOT NULL DEFAULT '0',
+  `area_name` varchar(30) DEFAULT NULL,
+  `sort_key` varchar(30) NOT NULL DEFAULT '',
+  `timezone` varchar(50) DEFAULT NULL,
+  `area_admin_email` text,
+  `resolution` int(11) DEFAULT NULL,
+  `default_duration` int(11) DEFAULT NULL,
+  `default_duration_all_day` tinyint(1) NOT NULL DEFAULT '0',
+  `morningstarts` int(11) DEFAULT NULL,
+  `morningstarts_minutes` int(11) DEFAULT NULL,
+  `eveningends` int(11) DEFAULT NULL,
+  `eveningends_minutes` int(11) DEFAULT NULL,
+  `private_enabled` tinyint(1) DEFAULT NULL,
+  `private_default` tinyint(1) DEFAULT NULL,
+  `private_mandatory` tinyint(1) DEFAULT NULL,
+  `private_override` varchar(32) DEFAULT NULL,
+  `min_create_ahead_enabled` tinyint(1) DEFAULT NULL,
+  `min_create_ahead_secs` int(11) DEFAULT NULL,
+  `max_create_ahead_enabled` tinyint(1) DEFAULT NULL,
+  `max_create_ahead_secs` int(11) DEFAULT NULL,
+  `min_delete_ahead_enabled` tinyint(1) DEFAULT NULL,
+  `min_delete_ahead_secs` int(11) DEFAULT NULL,
+  `max_delete_ahead_enabled` tinyint(1) DEFAULT NULL,
+  `max_delete_ahead_secs` int(11) DEFAULT NULL,
+  `max_per_day_enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `max_per_day` int(11) NOT NULL DEFAULT '0',
+  `max_per_week_enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `max_per_week` int(11) NOT NULL DEFAULT '0',
+  `max_per_month_enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `max_per_month` int(11) NOT NULL DEFAULT '0',
+  `max_per_year_enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `max_per_year` int(11) NOT NULL DEFAULT '0',
+  `max_per_future_enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `max_per_future` int(11) NOT NULL DEFAULT '0',
+  `max_duration_enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `max_duration_secs` int(11) NOT NULL DEFAULT '0',
+  `max_duration_periods` int(11) NOT NULL DEFAULT '0',
+  `custom_html` text,
+  `approval_enabled` tinyint(1) DEFAULT NULL,
+  `reminders_enabled` tinyint(1) DEFAULT NULL,
+  `enable_periods` tinyint(1) DEFAULT NULL,
+  `confirmation_enabled` tinyint(1) DEFAULT NULL,
+  `confirmed_default` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `mrbs_area`
+--
+
+INSERT INTO `mrbs_area` (`id`, `disabled`, `area_name`, `sort_key`, `timezone`, `area_admin_email`, `resolution`, `default_duration`, `default_duration_all_day`, `morningstarts`, `morningstarts_minutes`, `eveningends`, `eveningends_minutes`, `private_enabled`, `private_default`, `private_mandatory`, `private_override`, `min_create_ahead_enabled`, `min_create_ahead_secs`, `max_create_ahead_enabled`, `max_create_ahead_secs`, `min_delete_ahead_enabled`, `min_delete_ahead_secs`, `max_delete_ahead_enabled`, `max_delete_ahead_secs`, `max_per_day_enabled`, `max_per_day`, `max_per_week_enabled`, `max_per_week`, `max_per_month_enabled`, `max_per_month`, `max_per_year_enabled`, `max_per_year`, `max_per_future_enabled`, `max_per_future`, `max_duration_enabled`, `max_duration_secs`, `max_duration_periods`, `custom_html`, `approval_enabled`, `reminders_enabled`, `enable_periods`, `confirmation_enabled`, `confirmed_default`) VALUES
+(1, 0, 'Colegio San Francisco de Asis', 'Colegio San Francisco de Asis', 'America/Santiago', NULL, 1800, 3600, 0, 7, 0, 18, 30, 0, 0, 0, 'none', 0, 0, 0, 604800, 0, 0, 0, 604800, 0, 1, 0, 5, 0, 10, 0, 50, 0, 100, 0, 7200, 2, NULL, 0, 1, 0, 1, 1),
+(2, 0, 'Anexo San Francisco de Asis', 'Anexo San Francisco de Asis', 'America/Santiago', NULL, 1800, 3600, 0, 7, 0, 18, 30, 0, 0, 0, 'none', 0, 0, 0, 604800, 0, 0, 0, 604800, 0, 1, 0, 5, 0, 10, 0, 50, 0, 100, 0, 7200, 2, NULL, 0, 1, 0, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mrbs_entry`
+--
+
+CREATE TABLE `mrbs_entry` (
+  `id` int(11) NOT NULL,
+  `start_time` int(11) NOT NULL DEFAULT '0',
+  `end_time` int(11) NOT NULL DEFAULT '0',
+  `entry_type` int(11) NOT NULL DEFAULT '0',
+  `repeat_id` int(11) DEFAULT NULL,
+  `room_id` int(11) NOT NULL DEFAULT '1',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_by` varchar(80) NOT NULL DEFAULT '',
+  `modified_by` varchar(80) NOT NULL DEFAULT '',
+  `name` varchar(80) NOT NULL DEFAULT '',
+  `type` char(1) NOT NULL DEFAULT 'E',
+  `description` text,
+  `status` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
+  `reminded` int(11) DEFAULT NULL,
+  `info_time` int(11) DEFAULT NULL,
+  `info_user` varchar(80) DEFAULT NULL,
+  `info_text` text,
+  `ical_uid` varchar(255) NOT NULL DEFAULT '',
+  `ical_sequence` smallint(6) NOT NULL DEFAULT '0',
+  `ical_recur_id` varchar(16) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `mrbs_entry`
+--
+
+INSERT INTO `mrbs_entry` (`id`, `start_time`, `end_time`, `entry_type`, `repeat_id`, `room_id`, `timestamp`, `create_by`, `modified_by`, `name`, `type`, `description`, `status`, `reminded`, `info_time`, `info_user`, `info_text`, `ical_uid`, `ical_sequence`, `ical_recur_id`) VALUES
+(6, 1488798000, 1488803400, 1, 2, 3, '2017-03-04 00:54:55', 'betho', 'betho', 'Sala de computacion', 'I', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas \"Letraset\", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.', 0, NULL, NULL, NULL, NULL, 'MRBS-58ba1022795af-d1db636d@localhost', 1, '20170306T110000Z'),
+(7, 1488884400, 1488889800, 1, 2, 3, '2017-03-04 00:54:55', 'betho', 'betho', 'Sala de computacion', 'I', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas \"Letraset\", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.', 0, NULL, NULL, NULL, NULL, 'MRBS-58ba1022795af-d1db636d@localhost', 1, '20170307T110000Z'),
+(8, 1488970800, 1488976200, 1, 2, 3, '2017-03-04 00:54:55', 'betho', 'betho', 'Sala de computacion', 'I', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas \"Letraset\", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.', 0, NULL, NULL, NULL, NULL, 'MRBS-58ba1022795af-d1db636d@localhost', 1, '20170308T110000Z'),
+(9, 1489057200, 1489062600, 1, 2, 3, '2017-03-04 00:54:55', 'betho', 'betho', 'Sala de computacion', 'I', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas \"Letraset\", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.', 0, NULL, NULL, NULL, NULL, 'MRBS-58ba1022795af-d1db636d@localhost', 1, '20170309T110000Z'),
+(10, 1489143600, 1489149000, 1, 2, 3, '2017-03-04 00:54:55', 'betho', 'betho', 'Sala de computacion', 'I', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas \"Letraset\", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.', 0, NULL, NULL, NULL, NULL, 'MRBS-58ba1022795af-d1db636d@localhost', 1, '20170310T110000Z');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mrbs_repeat`
+--
+
+CREATE TABLE `mrbs_repeat` (
+  `id` int(11) NOT NULL,
+  `start_time` int(11) NOT NULL DEFAULT '0',
+  `end_time` int(11) NOT NULL DEFAULT '0',
+  `rep_type` int(11) NOT NULL DEFAULT '0',
+  `end_date` int(11) NOT NULL DEFAULT '0',
+  `rep_opt` varchar(32) NOT NULL DEFAULT '',
+  `room_id` int(11) NOT NULL DEFAULT '1',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_by` varchar(80) NOT NULL DEFAULT '',
+  `modified_by` varchar(80) NOT NULL DEFAULT '',
+  `name` varchar(80) NOT NULL DEFAULT '',
+  `type` char(1) NOT NULL DEFAULT 'E',
+  `description` text,
+  `rep_num_weeks` smallint(6) DEFAULT NULL,
+  `month_absolute` smallint(6) DEFAULT NULL,
+  `month_relative` varchar(4) DEFAULT NULL,
+  `status` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
+  `reminded` int(11) DEFAULT NULL,
+  `info_time` int(11) DEFAULT NULL,
+  `info_user` varchar(80) DEFAULT NULL,
+  `info_text` text,
+  `ical_uid` varchar(255) NOT NULL DEFAULT '',
+  `ical_sequence` smallint(6) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `mrbs_repeat`
+--
+
+INSERT INTO `mrbs_repeat` (`id`, `start_time`, `end_time`, `rep_type`, `end_date`, `rep_opt`, `room_id`, `timestamp`, `create_by`, `modified_by`, `name`, `type`, `description`, `rep_num_weeks`, `month_absolute`, `month_relative`, `status`, `reminded`, `info_time`, `info_user`, `info_text`, `ical_uid`, `ical_sequence`) VALUES
+(2, 1488798000, 1488803400, 1, 1489143600, '\'0\'', 3, '2017-03-04 00:54:55', 'betho', 'betho', 'Sala de computacion', 'I', 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas \"Letraset\", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.', 1, NULL, NULL, 0, NULL, NULL, NULL, NULL, 'MRBS-58ba1022795af-d1db636d@localhost', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mrbs_room`
+--
+
+CREATE TABLE `mrbs_room` (
+  `id` int(11) NOT NULL,
+  `disabled` tinyint(1) NOT NULL DEFAULT '0',
+  `area_id` int(11) NOT NULL DEFAULT '0',
+  `room_name` varchar(25) NOT NULL DEFAULT '',
+  `sort_key` varchar(25) NOT NULL DEFAULT '',
+  `description` varchar(60) DEFAULT NULL,
+  `capacity` int(11) NOT NULL DEFAULT '0',
+  `room_admin_email` text,
+  `custom_html` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `mrbs_room`
+--
+
+INSERT INTO `mrbs_room` (`id`, `disabled`, `area_id`, `room_name`, `sort_key`, `description`, `capacity`, `room_admin_email`, `custom_html`) VALUES
+(2, 0, 1, 'Sala de computacion 1', 'Sala de computacion 1', '', 20, 'ci.delpino@gmail.com', NULL),
+(3, 0, 2, 'Sala de computacion Anexo', 'Sala de computacion Anexo', '', 14, 'ci.delpino@gmail.com', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mrbs_users`
+--
+
+CREATE TABLE `mrbs_users` (
+  `id` int(11) NOT NULL,
+  `level` smallint(6) NOT NULL DEFAULT '0',
+  `name` varchar(30) DEFAULT NULL,
+  `password_hash` varchar(255) DEFAULT NULL,
+  `email` varchar(75) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `mrbs_users`
+--
+
+INSERT INTO `mrbs_users` (`id`, `level`, `name`, `password_hash`, `email`) VALUES
+(1, 2, 'betho', '$2y$10$CMY8rk8ixjaC.HUYWAK5IOJmr7bEffGpxwyYL0kBep8i4YJih//q.', 'ru.bugueno@gmail.com'),
+(2, 1, 'cinthya', '$2y$10$SVr6U/2r.PSUeCIAXy6xCeTno731HNQ3as.nrAAqqj1KxhRn.WjY2', 'ci.delpinog@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mrbs_variables`
+--
+
+CREATE TABLE `mrbs_variables` (
+  `id` int(11) NOT NULL,
+  `variable_name` varchar(80) DEFAULT NULL,
+  `variable_content` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `mrbs_variables`
+--
+
+INSERT INTO `mrbs_variables` (`id`, `variable_name`, `variable_content`) VALUES
+(1, 'db_version', '49'),
+(2, 'local_db_version', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mrbs_zoneinfo`
+--
+
+CREATE TABLE `mrbs_zoneinfo` (
+  `id` int(11) NOT NULL,
+  `timezone` varchar(255) NOT NULL DEFAULT '',
+  `outlook_compatible` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
+  `vtimezone` text,
+  `last_updated` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `mrbs_zoneinfo`
+--
+
+INSERT INTO `mrbs_zoneinfo` (`id`, `timezone`, `outlook_compatible`, `vtimezone`, `last_updated`) VALUES
+(1, 'America/Santiago', 1, 'BEGIN:VTIMEZONE\r\nTZID:America/Santiago\r\nTZURL:http://tzurl.org/zoneinfo-outlook/America/Santiago\r\nX-LIC-LOCATION:America/Santiago\r\nBEGIN:STANDARD\r\nTZOFFSETFROM:-0300\r\nTZOFFSETTO:-0300\r\nTZNAME:CLT\r\nDTSTART:19700101T000000\r\nEND:STANDARD\r\nEND:VTIMEZONE', 1488588741);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `mrbs_area`
+--
+ALTER TABLE `mrbs_area`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_area_name` (`area_name`);
+
+--
+-- Indices de la tabla `mrbs_entry`
+--
+ALTER TABLE `mrbs_entry`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `room_id` (`room_id`),
+  ADD KEY `repeat_id` (`repeat_id`),
+  ADD KEY `idxStartTime` (`start_time`),
+  ADD KEY `idxEndTime` (`end_time`);
+
+--
+-- Indices de la tabla `mrbs_repeat`
+--
+ALTER TABLE `mrbs_repeat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `room_id` (`room_id`);
+
+--
+-- Indices de la tabla `mrbs_room`
+--
+ALTER TABLE `mrbs_room`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_room_name` (`area_id`,`room_name`),
+  ADD KEY `idxSortKey` (`sort_key`);
+
+--
+-- Indices de la tabla `mrbs_users`
+--
+ALTER TABLE `mrbs_users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_name` (`name`);
+
+--
+-- Indices de la tabla `mrbs_variables`
+--
+ALTER TABLE `mrbs_variables`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_variable_name` (`variable_name`);
+
+--
+-- Indices de la tabla `mrbs_zoneinfo`
+--
+ALTER TABLE `mrbs_zoneinfo`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_timezone` (`timezone`,`outlook_compatible`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `mrbs_area`
+--
+ALTER TABLE `mrbs_area`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `mrbs_entry`
+--
+ALTER TABLE `mrbs_entry`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT de la tabla `mrbs_repeat`
+--
+ALTER TABLE `mrbs_repeat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `mrbs_room`
+--
+ALTER TABLE `mrbs_room`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `mrbs_users`
+--
+ALTER TABLE `mrbs_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `mrbs_variables`
+--
+ALTER TABLE `mrbs_variables`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `mrbs_zoneinfo`
+--
+ALTER TABLE `mrbs_zoneinfo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `mrbs_entry`
+--
+ALTER TABLE `mrbs_entry`
+  ADD CONSTRAINT `mrbs_entry_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `mrbs_room` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `mrbs_entry_ibfk_2` FOREIGN KEY (`repeat_id`) REFERENCES `mrbs_repeat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `mrbs_repeat`
+--
+ALTER TABLE `mrbs_repeat`
+  ADD CONSTRAINT `mrbs_repeat_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `mrbs_room` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `mrbs_room`
+--
+ALTER TABLE `mrbs_room`
+  ADD CONSTRAINT `mrbs_room_ibfk_1` FOREIGN KEY (`area_id`) REFERENCES `mrbs_area` (`id`) ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
